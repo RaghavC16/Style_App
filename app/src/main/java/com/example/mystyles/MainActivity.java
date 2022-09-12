18040f315fff83b2ea5a854c6da0ee25cbf43c7d
RaghavC16 <50890157+RaghavC16@users.noreply.github.com>
2022-09-12 10:33:39 -0500

    added all the basic required elements for padding

--- app/src/main/java/com/example/mystyles/MainActivity.java
+++ app/src/main/java/com/example/mystyles/MainActivity.java
@@ -16,25 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    TextView text = findViewById(R.id.finaltext);
-
-    public void hitButton(View v){
-        // get a reference to the EditText element in the xml page that is connected to this activity.
-        // to this activity
-        EditText editText1 = findViewById(R.id.name);
-        EditText editText2 = findViewById(R.id.day);
-        // extract the text
-        String input1 = editText1.getText().toString();
-        String input2 = editText2.getText().toString();
-
-        String output = "Hi " + input1 + " I hope you have a great " + editText2 + "!";
-
-        // I learned how to use the Double.toString to change the double answer into a string to display on the second screen.
-        text.setText(output);
-        // launches the new screen and passes the information
 
 
-    }
 
 
 }
